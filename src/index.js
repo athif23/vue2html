151cb5bcea70b6074bdb80a897d8f9523d10b450
Athif Humam <emailta.indo@gmail.com>
2020-06-01 03:29:24 +0800

    fix: 🐛 comment out console.log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const compileToHTML = require('./compile-to-html.js');
 const compileToComponent = require('./compile-to-component.js');
 
-compileToHTML('./test/fixtures/App.vue', {props: {name: 'Athif Humam'}}).then(console.log);
+// CompileToHTML('./test/fixtures/App.vue', {props: {name: 'Athif Humam'}}).then(console.log);
 
 module.exports = {
     compileToHTML,
