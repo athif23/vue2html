cea9a78271ab847ccfd1c5e75cac53ca5deb8e4a
Athif Humam <emailta.indo@gmail.com>
2020-06-02 19:10:00 +0800

    refactor: 💡 remove comment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const compileToHTML = require('./compile-to-html.js');
 const compileToComponent = require('./compile-to-component.js');
 
-// CompileToHTML('./test/fixtures/App.vue', {props: {name: 'Athif Humam'}}).then(console.log);
-
 module.exports = {
     compileToHTML,
     compileToComponent
