d0b7f7dcdf9e128d7ba0bc7755185fb4a14061dd
Athif Humam <emailta.indo@gmail.com>
2020-06-07 20:10:34 +0800

    feat: 🎸 minify package using gzip and terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
+import { brotliCompressSync } from 'zlib';
+import { terser } from "rollup-plugin-terser";
+import gzipPlugin from 'rollup-plugin-gzip';
 
 export default {
     input: ['src/index.js', 'src/cli.js'],
@@ -6,5 +9,12 @@ export default {
         dir: 'dist',
         format: 'cjs'
     },
-    plugins: [resolve()]
+    plugins: [
+	resolve(), 
+	terser(),
+	gzipPlugin({
+		customCompression: content =>
+		       brotliCompressSync(Buffer.from(content)), fileName: '.br'
+	})
+    ]
 };
